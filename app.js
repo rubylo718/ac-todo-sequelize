f602df454b9d9c921465eb9a27d765e770f8500a
rubylo718 <rubylo718@gmail.com>
2022-07-03 13:07:00 +0800

    feat: add index page to show the data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const exphbs = require('express-handlebars')
+const helpers = require('handlebars-helpers')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 
@@ -12,14 +13,24 @@ const User = db.User
 
 const port = process.env.PORT
 const app = express()
+const multihelpers = helpers()
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ helpers: multihelpers, defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then(todos => {
+      return res.render('index', { todos: todos })
+    })
+    .catch(error => {
+      return res.status(422).json(error)
+    })
 })
 
 app.get('/users/login', (req, res) => {
