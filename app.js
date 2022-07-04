f3107f5ee97bfe0f468f08b38ad55f5ae6e2d719
rubylo718 <rubylo718@gmail.com>
2022-07-04 21:44:39 +0800

    feat: add user auth function

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const helpers = require('handlebars-helpers')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
+const usePassport = require('./config/passport')
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
@@ -17,6 +20,13 @@ const multihelpers = helpers()
 
 app.engine('hbs', exphbs({ helpers: multihelpers, defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
@@ -37,9 +47,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: 'users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
