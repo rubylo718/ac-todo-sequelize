b2a23eb209edf65f09e693739a3d63d7b1c43387
rubylo718 <rubylo718@gmail.com>
2022-07-06 15:21:03 +0800

    feat: add flash message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const helpers = require('handlebars-helpers')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const usePassport = require('./config/passport')
 const routes = require('./routes')
@@ -28,9 +29,12 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
